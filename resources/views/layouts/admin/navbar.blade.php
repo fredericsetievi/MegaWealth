a8628f6682b16bca686f6bd5c5ae69431fde04c9
fredericsetievi <fredericsetievi@gmail.com>
2022-04-15 00:47:46 +0700

    change admin navbar

--- resources/views/layouts/admin/navbar.blade.php
+++ resources/views/layouts/admin/navbar.blade.php
@@ -1,24 +1,24 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg" style="background-color: #0e185f;">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">megAWealth</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        <a class="navbar-brand link-light" href="/">megAWealth</a>
+        <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon">=</span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Home</a>
+                    <a class="nav-link link-light" href="#">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('manageCompanyPage') }}">Manage Company</a>
+                    <a class="nav-link link-light" href="{{ route('manageCompanyPage') }}">Manage Company</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('realEstatePage') }}">Manage Real Estate
+                    <a class="nav-link link-light" href="{{ route('realEstatePage') }}">Manage Real Estate
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('logout') }}">Logout</a>
+                    <a class="nav-link link-light" href="{{ route('logout') }}">Logout</a>
                 </li>
             </ul>
         </div>
--- resources/views/layouts/user/navbar.blade.php
+++ resources/views/layouts/user/navbar.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-expand-lg" style="background-color: #0e185f;">
     <div class="container-fluid">
         <a class="navbar-brand link-light" href="/">megAWealth</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon">=</span>
         </button>
         <div class="collapse navbar-collapse justify-content-end" id="navbarNav" style="color: #354696">
             <ul class="navbar-nav">
