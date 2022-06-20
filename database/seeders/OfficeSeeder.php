99e7f0a891d865c8fcb4fa4e54f0edc64f0cf95e
AndreasRonal <andreas.ronal@binus.ac.id>
2022-06-20 12:26:09 +0700

    adding dummy

--- database/seeders/OfficeSeeder.php
+++ database/seeders/OfficeSeeder.php
@@ -6,7 +6,6 @@ use App\Models\Office;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
-
 class OfficeSeeder extends Seeder
 {
     /**
@@ -17,12 +16,102 @@ class OfficeSeeder extends Seeder
     public function run()
     {
         DB::table(table: "offices")->insert([
-            'id' => Str::uuid(),
+            "id" => Str::uuid(),
+            "name" => "Cikurut Office",
+            "address" => "Jl. Cikurut No. 4, Cikurut, Kota Bandung, Jawa Barat 40133",
+            "contactName" => "Anton Surya",
+            "phone" => "089614568833",
+            "image" => 'office11.jpg',
+        ]);
+
+        Office::create([
+            "id" => Str::uuid(),
             "name" => "Cikutra Office",
             "address" => "Jl. Cikutra No. 1, Cikutra, Kota Bandung, Jawa Barat 40132",
             "contactName" => "Jaya Kusuma",
             "phone" => "089614568840",
-            "image" => '',
+            "image" => 'office1.jpg'
+        ]);
+
+        Office::create([
+            "id" => Str::uuid(),
+            "name" => "Ciputra Office",
+            "address" => "Jl. Ciputra No. 1, Ciputra, Jakarta Barat, DKI Jakarta 11432",
+            "contactName" => "Budi Andri",
+            "phone" => "081808188888",
+            "image" => 'office2.jpg'
+        ]);
+
+        Office::create([
+            "id" => Str::uuid(),
+            "name" => "BSD Office",
+            "address" => "Jl. BSD No. 1, Serpong, Tangerang Selatan, Banten 21311",
+            "contactName" => "Kesuma",
+            "phone" => "082233333333",
+            "image" => 'office3.jpg'
+        ]);
+
+        Office::create([
+            "id" => Str::uuid(),
+            "name" => "Pluit Office",
+            "address" => "Jl. Pluit Raya No. 2, Pluit, Jakarta Utara, DKI Jakarta 31231",
+            "contactName" => "Kolawit",
+            "phone" => "082123134567",
+            "image" => 'office4.jpg'
+        ]);
+
+        Office::create([
+            "id" => Str::uuid(),
+            "name" => "Percetakan Office",
+            "address" => "Jl. Percetakan Negara 1 No. 3, Cempaka Putih, Jakarta Pusat, DKI Jakarta 12345",
+            "contactName" => "Ronald",
+            "phone" => "081212122133",
+            "image" => 'office5.jpg'
+        ]);
+
+        Office::create([
+            "id" => Str::uuid(),
+            "name" => "Kemang Office",
+            "address" => "Jl. Kemang Raya No. 4, Kemang, Jakarta Selatan, DKI Jakarta 21322",
+            "contactName" => "Ariana",
+            "phone" => "082245678901",
+            "image" => 'office6.jpg'
+        ]);
+
+        Office::create([
+            "id" => Str::uuid(),
+            "name" => "Bungur Office",
+            "address" => "Jl. Bungur No. 3, Mampang, Jakarta Selatan, DKI Jakarta 21322",
+            "contactName" => "Chandra",
+            "phone" => "081825409876",
+            "image" => 'office7.jpg'
+        ]);
+
+        Office::create([
+            "id" => Str::uuid(),
+            "name" => "Foresta 1 Office",
+            "address" => "Jl. Pagedangan No. 3, Pagedangan, Tangerang, Banten 21311",
+            "contactName" => "Siti",
+            "phone" => "082345678910",
+            "image" => 'office8.jpg'
+        ]);
+
+        Office::create([
+            "id" => Str::uuid(),
+            "name" => "Foresta 2 Office",
+            "address" => "Jl. Pagedangan No. 6, Pagedangan, Tangerang, Banten 21311",
+            "contactName" => "Nur Azizah",
+            "phone" => "085678904568",
+            "image" => 'office9.jpg'
+        ]);
+
+        Office::create([
+            "id" => Str::uuid(),
+            "name" => "Menara Office",
+            "address" => "Jl. M.H. Thamrin No. 1, Tanah Abang, Jakarta Pusat, DKI Jakarta 12346",
+            "contactName" => "Kesuma",
+            "phone" => "082233333333",
+            "image" => 'office10.jpg'
         ]);
 
         Office::factory(20)->create();
--- database/seeders/RealEstateSeeder.php
+++ database/seeders/RealEstateSeeder.php
@@ -22,7 +22,97 @@ class RealEstateSeeder extends Seeder
             "buildingType" => "Apartement",
             "price" => "500 / month",
             "location" => "Meikarta, Jakarta",
-            "image" => '',
+            "image" => 'real11.jpg',
+        ]);
+
+        RealEstate::create([
+            'id' => Str::uuid(),
+            "salesType" => "Rent",
+            "buildingType" => "House",
+            "price" => "700 / month",
+            "location" => "Pluit, Jakarta",
+            "image" => 'real1.jpg',
+        ]);
+
+        RealEstate::create([
+            'id' => Str::uuid(),
+            "salesType" => "Rent",
+            "buildingType" => "Apartment",
+            "price" => "600 / month",
+            "location" => "PIK 1, Jakarta",
+            "image" => 'real4.jpg',
+        ]);
+
+        RealEstate::create([
+            'id' => Str::uuid(),
+            "salesType" => "Rent",
+            "buildingType" => "Apartment",
+            "price" => "400 / month",
+            "location" => "Rajawali, Jakarta",
+            "image" => 'real6.jpg',
+        ]);
+
+        RealEstate::create([
+            'id' => Str::uuid(),
+            "salesType" => "Rent",
+            "buildingType" => "Apartment",
+            "price" => "1000 / month",
+            "location" => "Gandaria, Jakarta",
+            "image" => 'real10.jpg',
+        ]);
+
+        RealEstate::create([
+            'id' => Str::uuid(),
+            "salesType" => "Sale",
+            "buildingType" => "House",
+            "price" => "1300",
+            "location" => "Bungur, Jakarta",
+            "image" => 'real2.jpg',
+        ]);
+
+        RealEstate::create([
+            'id' => Str::uuid(),
+            "salesType" => "Sale",
+            "buildingType" => "House",
+            "price" => "1800",
+            "location" => "Pondok Indah, Jakarta",
+            "image" => 'real3.jpg',
+        ]);
+
+        RealEstate::create([
+            'id' => Str::uuid(),
+            "salesType" => "Sale",
+            "buildingType" => "House",
+            "price" => "2000",
+            "location" => "Pondok Indah, Jakarta",
+            "image" => 'real5.jpg',
+        ]);
+
+        RealEstate::create([
+            'id' => Str::uuid(),
+            "salesType" => "Sale",
+            "buildingType" => "House",
+            "price" => "2100",
+            "location" => "PIK 2, Jakarta",
+            "image" => 'real7.jpg',
+        ]);
+
+        RealEstate::create([
+            'id' => Str::uuid(),
+            "salesType" => "Sale",
+            "buildingType" => "Apartment",
+            "price" => "1600",
+            "location" => "Pluit, Jakarta",
+            "image" => 'real8.jpg',
+        ]);
+
+        RealEstate::create([
+            'id' => Str::uuid(),
+            "salesType" => "Sale",
+            "buildingType" => "House",
+            "price" => "1400",
+            "location" => "Sunter, Jakarta",
+            "image" => 'real9.jpg',
         ]);
 
         RealEstate::factory(50)->create();
